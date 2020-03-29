71a7ec7c2debd0c614f74ac77dea8614311c9d99
jagajastic <josephibrahi@gmail.com>
2020-03-29 11:03:10 +0100

    add inheritance to calengin

---
+++ src/com/company/Substracter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Substracter extends CalculateBase {
+    public Substracter() {}
+    public Substracter(double leftVal, double rightVal){
+        super(leftVal, rightVal);
+    }
+
+    @Override
+    public void calculate() {
+        double value = getLeftVal() - getRightVal();
+        setResult(value);
+    }
+}
